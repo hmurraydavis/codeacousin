e466927785137c6d3499fd28d3bef539b209aa6d
Halie <hmurraydavis@users.noreply.github.com>
2013-12-21 19:45:35 -0500

    Reads from the photoresistor

--- LEDandPotfun.ino
+++ LEDandPotfun.ino
@@ -14,16 +14,26 @@ String getmode(){
 	return content;
 }
 
+float photodata(){
+	float photodata=analogRead(4);
+}
+
+void ledfun(){
+	
+}
+
 //declare global variables:
 int led1=1; //put an led on pin 1
 int led2=2; //put an led on pin 2
 int led3=3; //put an led on pin 3
+int photores=4; //put a photoresistor on pin 4
 
 void setup(){
 	Serial.begin(9600);
 	for (int lednum=1; lednum<=3; lednum++){
 		pinMode (lednum, OUTPUT);
 	}
+	pinMode(photores, INPUT);
 }
 
 void loop(){
